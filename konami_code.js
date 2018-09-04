30caa5d0eb491cb8ee1c02f9a2d9bec58b723683
Carmela Torres Molleda <carmela.torres@wework.com>
2018-09-04 18:43:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,30 +1,16 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65] 
 let index = 0;
-
-function init() {
-  // Write your JavaScript code inside the init() function
-  document.body.addEventListener("keydown", function (e) {
-   (e.which); 
-  // const key =e.which;
-   const key = parseInt(e.detail || e.which);
-   console.log(key);
-   if (key === code[index]) {
-     index++;
-     console.log(index);
-     if (index === code.length) {
-       alert("Hurray!");
-       console.log(index);
-       index = 0;
-     }
-   }
-   
-   
-    else {
+function init(){
+  const input = document.body;
+  input.addEventListener('keydown', e => {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    }else {
       index = 0;
-      console.log(index);
     }
-  
   });
-  
- }
- init();
